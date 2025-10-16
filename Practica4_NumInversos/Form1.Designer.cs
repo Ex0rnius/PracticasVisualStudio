9ecadf7493394ee9cf152addae2e7527585e477c
Ex0rnius <nixer974@gmail.com>
2025-10-16 09:49:43 -0600

    Add files via upload

---
+++ Practica4_NumInversos/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Practica4_NumInversos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(59, 74);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Número A";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(60, 129);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Número B";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(60, 187);
+            label3.Name = "label3";
+            label3.Size = new Size(87, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Número C";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(58, 240);
+            label4.Name = "label4";
+            label4.Size = new Size(89, 21);
+            label4.TabIndex = 3;
+            label4.Text = "Número D";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(172, 9);
+            label5.Name = "label5";
+            label5.Size = new Size(452, 37);
+            label5.TabIndex = 4;
+            label5.Text = "Escribir números en orden inverso";
+            label5.Click += label5_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(321, 335);
+            button1.Name = "button1";
+            button1.Size = new Size(152, 90);
+            button1.TabIndex = 5;
+            button1.Text = "Invertir Orden";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(172, 76);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(147, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(172, 127);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(147, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(172, 185);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(147, 23);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(172, 238);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(147, 23);
+            textBox4.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(385, 74);
+            label6.Name = "label6";
+            label6.Size = new Size(88, 21);
+            label6.TabIndex = 10;
+            label6.Text = "Número A";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(386, 129);
+            label7.Name = "label7";
+            label7.Size = new Size(87, 21);
+            label7.TabIndex = 11;
+            label7.Text = "Número B";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(386, 187);
+            label8.Name = "label8";
+            label8.Size = new Size(87, 21);
+            label8.TabIndex = 12;
+            label8.Text = "Número C";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(386, 240);
+            label9.Name = "label9";
+            label9.Size = new Size(89, 21);
+            label9.TabIndex = 13;
+            label9.Text = "Número D";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(491, 76);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(147, 23);
+            textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(491, 185);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(147, 23);
+            textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(491, 131);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(147, 23);
+            textBox7.TabIndex = 16;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(491, 242);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(147, 23);
+            textBox8.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.grid_paper_pattern_background_vector;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Invertir Numeros";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+    }
+}
